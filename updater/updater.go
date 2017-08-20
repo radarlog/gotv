9bbd53cd6dc588e5c0a920d51ab8f4b0475ece60
Ilian Ranguelov <me@radarlog.net>
2017-08-20 23:32:07 +0200

    retrieve html

--- updater/updater.go
+++ updater/updater.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+    "net/http"
+    "io/ioutil"
+    "fmt"
+)
+
+const (
+    mezzoRequestUrl string = "http://suppor6k.bget.ru/onelike/mezzo.php"
+    mezzoRefererUrl string = "http://onelike.tv/mezzo.html"
+)
 
 func main() {
-    fmt.Printf("hello, world\n")
+    html := getBody(mezzoRequestUrl, mezzoRefererUrl)
+
+    fmt.Print(string(html))
+}
+
+func getBody(url string, referer string) []byte {
+    client := http.Client{}
+
+    request, err := http.NewRequest("GET", url, nil)
+    if err != nil {
+        panic(err)
+    }
+
+    request.Header.Set("Referer", referer)
+
+    response, err := client.Do(request)
+    if err != nil {
+        panic(err)
+    }
+
+    defer response.Body.Close()
+
+    body, err := ioutil.ReadAll(response.Body)
+    if err != nil {
+        panic(err)
+    }
+
+    return body
 }
