b018649ae76c6a6367c38773a15f4632783a6201
Ilian Ranguelov <me@radarlog.net>
2017-08-27 14:18:25 +0200

    go fmt

--- updater/updater.go
+++ updater/updater.go
@@ -1,45 +1,45 @@
 package main
 
 import (
-    "fmt"
-    "io/ioutil"
-    "net/http"
-    "regexp"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"regexp"
 )
 
 const (
-    mezzoRequestUrl  string = "http://suppor6k.bget.ru/onelike/mezzo.php"
-    mezzoRefererUrl  string = "http://onelike.tv/mezzo.html"
-    streamUrlPattern string = "http://s2a.privit.pro:8081/mezzo/index.m3u8\\?wmsAuthSign=(.+==)"
+	mezzoRequestUrl  string = "http://suppor6k.bget.ru/onelike/mezzo.php"
+	mezzoRefererUrl  string = "http://onelike.tv/mezzo.html"
+	streamUrlPattern string = "http://s2a.privit.pro:8081/mezzo/index.m3u8\\?wmsAuthSign=(.+==)"
 )
 
 func main() {
-    body := getBody(mezzoRequestUrl, mezzoRefererUrl)
+	body := getBody(mezzoRequestUrl, mezzoRefererUrl)
 
-    streamUrl := getLink(body)
+	streamUrl := getLink(body)
 
-    fmt.Println(streamUrl)
+	fmt.Println(streamUrl)
 }
 
 func getBody(url string, referer string) string {
-    request, _ := http.NewRequest("GET", url, nil)
-    request.Header.Set("Referer", referer)
+	request, _ := http.NewRequest("GET", url, nil)
+	request.Header.Set("Referer", referer)
 
-    client := http.Client{}
-    resp, _ := client.Do(request)
-    defer resp.Body.Close()
+	client := http.Client{}
+	resp, _ := client.Do(request)
+	defer resp.Body.Close()
 
-    if resp.StatusCode == 200 {
-        bodyBytes, _ := ioutil.ReadAll(resp.Body)
-        return string(bodyBytes)
-    }
+	if resp.StatusCode == 200 {
+		bodyBytes, _ := ioutil.ReadAll(resp.Body)
+		return string(bodyBytes)
+	}
 
-    return ""
+	return ""
 }
 
 func getLink(body string) string {
-    r, _ := regexp.Compile(streamUrlPattern)
-    streamUrl := r.FindString(body)
+	r, _ := regexp.Compile(streamUrlPattern)
+	streamUrl := r.FindString(body)
 
-    return streamUrl
+	return streamUrl
 }
