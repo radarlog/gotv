23bbfb4f7e7519932f2bd637a35d5bd7f06d503d
Ilian Ranguelov <me@radarlog.net>
2017-10-01 16:01:33 +0200

    add meta flag

--- updater/updater.go
+++ updater/updater.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 )
 
 func main() {
-	config := Meta("src/github.com/radarlog/gotv/meta.yml")
+	metaFile := flag.String("m", "meta.yml", "meta file to read configuration from")
+	flag.Parse()
+
+	config := Meta(*metaFile)
 
 	fmt.Println(config.Tv.Onelike["mezzo"].Stream())
 	fmt.Println(config.Tv.Onelike["nat-geo-wild"].Stream())
