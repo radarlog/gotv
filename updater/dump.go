b7b4bdcabac7da62cf41a5bbe700cc69738e2c17
Ilian Ranguelov <me@radarlog.net>
2017-10-22 02:09:29 +0200

    dump logo

--- updater/dump.go
+++ updater/dump.go
@@ -2,30 +2,80 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 	"os"
+	"path/filepath"
 	"text/template"
 )
 
+type dump struct {
+	Logo   string
+	Name   string
+	Stream string
+}
+
 const tmpl = `#EXTM3U
 {{range .}}
-#EXTINF:0 tvg-logo="{{ .LogoUrl }}",{{ .Name }}
-{{ .PageUrl }}
+#EXTINF:0 tvg-logo="{{ .Logo }}",{{ .Name }}
+{{ .Stream }}
 {{end}}`
 
-func (config *meta) dump(file string) error {
+func (config *meta) dump(file string) (err error) {
 	t, err := template.New("playlist").Parse(tmpl)
 	if err != nil {
-		return err
+		return
 	}
 
 	// Open a new file for writing only
 	f, err := os.OpenFile(file, os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0644)
 	if err != nil {
-		return err
+		return
 	}
 	defer f.Close()
 
-	fmt.Println(file)
+	dumpList := make([]dump, 0)
+	for name, channel := range config.Channels {
+		logo, err := channel.dumpLogo(name, config.LogoDir)
+		if err != nil {
+			return err
+		}
 
-	return t.Execute(f, config.Channels)
+		dumpChannel := dump{
+			Logo:   logo,
+			Name:   channel.Name,
+			Stream: channel.PageUrl,
+		}
+
+		dumpList = append(dumpList, dumpChannel)
+	}
+
+	return t.Execute(f, dumpList)
+}
+
+func (c *Channel) dumpLogo(name string, dir string) (path string, err error) {
+	// create logo dir
+	err = os.MkdirAll(dir, os.ModePerm)
+	if err != nil {
+		return
+	}
+
+	// file name
+	path = fmt.Sprintf("%s/%s%s", dir, name, filepath.Ext(c.LogoUrl))
+
+	// create logo file
+	file, err := os.Create(path)
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	// download logo
+	response, err := http.Get(c.LogoUrl)
+	defer response.Body.Close()
+
+	// write downloaded logo to created file
+	_, err = io.Copy(file, response.Body)
+
+	return
 }
