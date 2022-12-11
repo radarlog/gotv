a7948f9171150d2c6f30638a8af093d4dfbfb647
Ilian Ranguelov <me@radarlog.net>
2022-12-11 21:32:35 +0100

    update onlytv api

--- plugins/onlytv.go
+++ plugins/onlytv.go
@@ -1,8 +1,8 @@
-// Package onlytv is a gotv plugin parsing channels on http://only-tv.org/
-//
+// Package onelike is a gotv's plugin implements parsing any channel on http://onelike.tv/
 package onlytv
 
 import (
+	"io/ioutil"
 	"log"
 	"net/http"
 	"regexp"
@@ -11,39 +11,34 @@ import (
 )
 
 // the regex pattern for finding stream out
-const streamUrlPattern = "file=(https?://.+?)\""
-
-// the user agent header used for making request
-const userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36"
+const streamUrlPattern = "file:\"(https?:\\/\\/.+)\""
 
 // find out channel's stream by parsing its page. All the pages are organized in the same way.
 // A jQuery video player which contains a wanted stream is inserted to the page inside iframe tag,
 // so fetching the stream is split into 2 steps: finding frame's URL and page's parsing by that URL
 func FindStream(pageUrl string) string {
 	frameUrl := getFrameUrl(pageUrl)
-	streamUrl := getStreamUrl(frameUrl)
+	streamUrl := getStreamUrl(frameUrl, pageUrl)
 
 	return streamUrl
 }
 
 // find out an URL of the iframe
 func getFrameUrl(pageUrl string) string {
-	html := request(pageUrl)
-
-	return html.Find("iframe").AttrOr("src", "")
-}
-
-// find out the stream inside iframe
-func getStreamUrl(frameUrl string) (matchedUrl string) {
-	html := request(frameUrl)
-
-	player, err := html.Find("div.player").Html()
+	html, err := goquery.NewDocument(pageUrl)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	return html.Find("iframe[name=frame]").AttrOr("src", "")
+}
+
+// find out the stream inside iframe
+func getStreamUrl(frameUrl string, referer string) (matchedUrl string) {
+	response := request(frameUrl, referer)
+
 	r := regexp.MustCompile(streamUrlPattern)
-	matched := r.FindStringSubmatch(player)
+	matched := r.FindStringSubmatch(response)
 
 	if len(matched) == 2 {
 		matchedUrl = matched[1]
@@ -52,15 +47,13 @@ func getStreamUrl(frameUrl string) (matchedUrl string) {
 	return
 }
 
-// URL is accessible only from channel's page, so it needs to be passed as a referer
-func request(url string) (doc *goquery.Document) {
+// iframe's URL is accessible only from channel's page, so it needs to be passed as a referer
+func request(url, referer string) (html string) {
 	request, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	request.Header.Set("Referer", url)
-	request.Header.Set("User-Agent", userAgent)
+	request.Header.Set("Referer", referer)
 
 	client := http.Client{}
 	resp, err := client.Do(request)
@@ -71,13 +64,13 @@ func request(url string) (doc *goquery.Document) {
 	defer resp.Body.Close()
 
 	if resp.StatusCode == http.StatusOK {
-		d, err := goquery.NewDocumentFromReader(resp.Body)
+		bodyBite, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		doc = d
+		html = string(bodyBite)
 	}
 
-	return
+	return html
 }
